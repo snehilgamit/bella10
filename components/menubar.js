3f06334835186aecca91e56216ecd3a05b47c152
snehil <108514128+snehilgamit@users.noreply.github.com>
2023-12-27 17:38:05 +0530

    update menubar.js

--- components/menubar.js
+++ components/menubar.js
@@ -15,7 +15,7 @@ const menubar = () => {
         if (type === 'show') {
             side_menu.style.right = '0%'
         }
-        else if(type ==='hide'){
+        else if (type === 'hide') {
             side_menu.style.right = '-100%'
         }
     }
@@ -71,7 +71,7 @@ const menubar = () => {
                         <div className='w-[110px] mt-2 min-h-[150px] absolute z-[1] cursor-pointer bg-black hidden menuitem_shop'>
                             <div>
                                 {SubmenuItmes.map((el, index) => (
-                                    <Link key={index} href={`/shop${el == 'all' ? '' : '?category='+el}`} className='menuitmes_sidebar_main text-sm py-2.5 mx-2 relative flex justify-between' >
+                                    <Link key={index} href={`/shop${el == 'all' ? '' : '?category=' + el}`} className='menuitmes_sidebar_main text-sm py-2.5 mx-2 relative flex justify-between' >
                                         <div className='mx-5'>{el}</div>
                                     </Link>
                                 ))}
@@ -88,7 +88,7 @@ const menubar = () => {
                 <div className='mr-6 flex justify-center items-center gap-5'>
                     <div className='cursor-pointer'><svg xmlns="http://www.w3.org/2000/svg" height="22" width="22" viewBox="0 0 448 512"><path d="M224 256A128 128 0 1 0 224 0a128 128 0 1 0 0 256zm-45.7 48C79.8 304 0 383.8 0 482.3C0 498.7 13.3 512 29.7 512H418.3c16.4 0 29.7-13.3 29.7-29.7C448 383.8 368.2 304 269.7 304H178.3z" /></svg>
                     </div>
-                    <div className='sidebar_icon cursor-pointer' onClick={()=>{showMenu('show')}}>
+                    <div className='sidebar_icon cursor-pointer' onClick={() => { showMenu('show') }}>
                         <div className='line'></div>
                         <div className='line'></div>
                         <div className='line'></div>
@@ -99,7 +99,7 @@ const menubar = () => {
                 <div className='menuitme_sidebar font-semibold'>
                     <div className='flex justify-between items-center py-4 bg-white shadow-md'>
                         <Link
-                            href={'/'} onClick={()=>{showMenu('hide')}}>
+                            href={'/'} onClick={() => { showMenu('hide') }}>
                             <Image
                                 alt='logo'
                                 className='ml-12 cursor-pointer'
@@ -109,12 +109,12 @@ const menubar = () => {
                                 loading='eager'
                             />
                         </Link>
-                        <div className='cursor-pointer mr-5 w-10 h-10 flex justify-center items-center' onClick={()=>{showMenu('hide')}}>
+                        <div className='cursor-pointer mr-5 w-10 h-10 flex justify-center items-center' onClick={() => { showMenu('hide') }}>
                             <div className='closeLine-x'></div>
                             <div className='closeLine-x'></div>
                         </div>
                     </div>
-                    <Link href={'/'} onClick={()=>{showMenu('hide')}} className='menuitmes_sidebar py-3.5 mx-2 menu_item relative border-b flex justify-between border-orange-200'>
+                    <Link href={'/'} onClick={() => { showMenu('hide') }} className='menuitmes_sidebar py-3.5 mx-2 menu_item relative border-b flex justify-between border-orange-200'>
                         <div className='mx-1 text-black'>Home</div>
                     </Link>
                     <div className='py-3.5 mx-2 relative border-b  border-orange-200'>
@@ -129,7 +129,7 @@ const menubar = () => {
                             <div className='m-2 mt-4'>
                                 <div>
                                     {SubmenuItmes.map((el, index) => (
-                                        <Link key={index} href={`/shop?category=${el}`} onClick={()=>{showMenu('hide')}} className='menuitmes_sidebar text-sm py-2 mx-2 relative border-t border-l border-r flex justify-between border-orange-200' style={{ 'border-bottom': el == 'all' ? '1px solid rgb(254 215 170)' : 'transparent' }}>
+                                        <Link key={index} href={`/shop${el == 'all' ? '' : '?category=' + el}`} onClick={() => { showMenu('hide') }} className='menuitmes_sidebar text-sm py-2 mx-2 relative border-t border-l border-r flex justify-between border-orange-200' style={{ 'border-bottom': el == 'all' ? '1px solid rgb(254 215 170)' : 'transparent' }}>
                                             <div className='mx-5 text-black'>{el}</div>
                                         </Link>
                                     ))}
@@ -137,16 +137,16 @@ const menubar = () => {
                             </div>
                         </div>
                     </div>
-                    <Link href={'/referral'} onClick={()=>{showMenu('hide')}} className='menuitmes_sidebar py-3.5 mx-2 menu_item relative border-b flex justify-between border-orange-200' >
+                    <Link href={'/referral'} onClick={() => { showMenu('hide') }} className='menuitmes_sidebar py-3.5 mx-2 menu_item relative border-b flex justify-between border-orange-200' >
                         <div className='mx-1 text-black'>Referral</div>
                     </Link>
-                    <Link href={'/location'} onClick={()=>{showMenu('hide')}} className='menuitmes_sidebar py-3.5 mx-2 menu_item relative border-b flex justify-between border-orange-200'>
+                    <Link href={'/location'} onClick={() => { showMenu('hide') }} className='menuitmes_sidebar py-3.5 mx-2 menu_item relative border-b flex justify-between border-orange-200'>
                         <div className='mx-1 text-black'>Location</div>
                     </Link>
-                    <Link href={'/offers'} onClick={()=>{showMenu('hide')}} className='menuitmes_sidebar py-3.5 mx-2 menu_item relative border-b flex justify-between border-orange-200' >
+                    <Link href={'/offers'} onClick={() => { showMenu('hide') }} className='menuitmes_sidebar py-3.5 mx-2 menu_item relative border-b flex justify-between border-orange-200' >
                         <div className='mx-1 text-black'>Offers</div>
                     </Link>
-                    <Link href={'/contact-us'} onClick={()=>{showMenu('hide')}} className='menuitmes_sidebar py-3.5 mx-2 menu_item relative border-b flex justify-between border-orange-200' >
+                    <Link href={'/contact-us'} onClick={() => { showMenu('hide') }} className='menuitmes_sidebar py-3.5 mx-2 menu_item relative border-b flex justify-between border-orange-200' >
                         <div className='mx-1 text-black'>Contact us</div>
                     </Link>
                     <div className='flex justify-center items-center loginArea'>

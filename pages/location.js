74fd16044745c0d8dbd5e7169610e8e7017a7b54
snehil <108514128+snehilgamit@users.noreply.github.com>
2023-12-27 17:50:56 +0530

    init

--- pages/location.js
+++ pages/location.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const location = () => {
   return (
-    <div>location</div>
+      <iframe  src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d3570.4254716629753!2d73.38945596385386!3d21.11072586496715!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3be07eb6e04ca381%3A0x30a46957fe570cd8!2sAARAM%20GUEST%20HOUSE!5e0!3m2!1sen!2sin!4v1703679599975!5m2!1sen!2sin" width="100%" height="650" style={{ border: '0' }} allowFullScreen="" loading="lazy" referrerPolicy="no-referrer-when-downgrade"></iframe>
   )
 }
 

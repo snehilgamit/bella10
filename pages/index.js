c9a4b864d6e2af5056ef0122b20f33de114df6dd
snehil <108514128+snehilgamit@users.noreply.github.com>
2023-12-28 17:07:09 +0530

    init

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,37 @@
-import React, { useEffect, useState } from 'react'
-import Menubar from '@/components/menubar'
+import React from 'react'
 import Image from 'next/image'
-import Head from 'next/head'
-const home = () => {
+import Link from 'next/link'
+const Homemenu = ["Cricket", "Football", "Tennis", "Volleyball", "All"];
+const Home = () => {
   return (
-    <>
-      <Head>
-        <title>hey</title>
-      </Head>
-      <div className='main_slide -mt-1 min-h-[90vh] w-full gap-10 flex'>
+    <div className='w-full overflow-hidden mb-10'>
+      <div className='offer_banner w-full h-full text-white max-sm:my-5 flex justify-center'>
+        <Image
+          src={'/5aa39a1c-e561-4055-9a39-0698075d2d99.webp'}
+          height={1000}
+          width={1600}
+          priority='eagar'
+        />
       </div>
-    </>
+      <div className='Home_shopmenu w-full h-full px-10 mt-8 font-semibold border rounded-3xl bg-orange-100'>
+        <div className='w-full text-5xl mt-6 max-sm:text-4xl max-sm:text-center'>
+          Shop
+        </div>
+        <div className='flex justify-start max-sm:justify-center items-center mt-6 flex-wrap pb-10 max-sm:gap-10 gap-14'>
+          {Homemenu.map((el, index) => (
+            <Link href={el=="All"?"/shop":`/shop?categories=${el}`} key={index} className='p-4 cursor-pointer px-2 flex justify-center items-center flex-col rounded-full max-sm:scale-90 max-sm:p-2'>
+              <Image
+                src={`/${el}.png`}
+                height={50}
+                width={50}
+              />
+              <span className='mt-1'>{el}</span>
+            </Link>
+          ))}
+        </div>
+      </div>
+    </div>
   )
 }
 
-export default home
+export default Home

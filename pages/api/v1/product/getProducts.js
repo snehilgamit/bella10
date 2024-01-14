d60d6eb1531b629a9e31fbad49a17b70b2128019
snehil <108514128+snehilgamit@users.noreply.github.com>
2024-01-14 20:52:17 +0530

    Update getProducts.js

--- pages/api/v1/product/getProducts.js
+++ pages/api/v1/product/getProducts.js
@@ -4,21 +4,28 @@ export default async function handler(req, res) {
     if (req.method == 'POST') {
         const { category } = req.body;
 
-        await connectDB();
-        const fetch = async (result) => {
-            const resResult = [];
-            result.forEach(el => {
-                resResult.push({
-                    product_id: el.product_id,
-                    name: el.name,
-                    price: el.price,
-                    percentage: el.percentage,
-                    image_uri: el.image_uri,
-                    price_after_discount: el.price_after_discount,
-                    category: el.category,
+        try {
+
+            await connectDB();
+            const fetch = async (result) => {
+                const resResult = [];
+                result.forEach(el => {
+                    resResult.push({
+                        product_id: el.product_id,
+                        name: el.name,
+                        price: el.price,
+                        percentage: el.percentage,
+                        image_uri: el.image_uri,
+                        price_after_discount: el.price_after_discount,
+                        category: el.category,
+                    });
                 });
-            });
-            return res.json({ status: true, results: resResult });
+                return res.json({ status: true, results: resResult });
+            }
+        }
+        catch (err) {
+            console.log(err);
+            return res.json({ status: false, message: "Invalid method" });
         }
         if (category === 'all') {
             const result = await products.find();

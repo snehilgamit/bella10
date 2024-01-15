0ff73a2fe89fd01bc4a194ca1348335e73609b18
snehil <108514128+snehilgamit@users.noreply.github.com>
2024-01-15 10:53:00 +0530

    Update getProducts.js

--- pages/api/v1/product/getProducts.js
+++ pages/api/v1/product/getProducts.js
@@ -1,24 +1,24 @@
 import products from "@/models/products";
 import connectDB from "@/util/mongoDB";
 export default async function handler(req, res) {
+    const fetch = async (result) => {
+        const resResult = [];
+        result.forEach(el => {
+            resResult.push({
+                product_id: el.product_id,
+                name: el.name,
+                percentage: el.percentage,
+                image_uri: el.image_uri,
+                price_after_discount: el.price_after_discount,
+                category: el.category,
+                price: el.price,
+            });
+        });
+        return res.json({ status: true, results: resResult });
+    }
     if (req.method == 'POST') {
         const { category } = req.body;
         await connectDB();
-        const fetch = async (result) => {
-            const resResult = [];
-            result.forEach(el => {
-                resResult.push({
-                    product_id: el.product_id,
-                    name: el.name,
-                    percentage: el.percentage,
-                    image_uri: el.image_uri,
-                    price_after_discount: el.price_after_discount,
-                    category: el.category,
-                    price: el.price,
-                });
-            });
-        return res.json({ status: true, results: resResult });
-        }
         if (category === 'all') {
             const result = await products.find();
             await fetch(result);

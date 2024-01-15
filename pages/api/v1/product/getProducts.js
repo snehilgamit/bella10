ca195952433a26f8bef7f91dff7ce78fe8db276b
snehil <108514128+snehilgamit@users.noreply.github.com>
2024-01-15 10:34:17 +0530

    Update getProducts.js

--- pages/api/v1/product/getProducts.js
+++ pages/api/v1/product/getProducts.js
@@ -23,7 +23,7 @@ export default async function handler(req, res) {
             const result = await products.find();
             await fetch(result);
         }
-        if (category) {
+        if (category !== "all") {
             const result = await products.find({ category });
             await fetch(result);
         }
